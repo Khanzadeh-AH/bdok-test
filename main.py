ac928f4a67921642095f941049d8e3b014d78fe2
AmirHossein <khanzadeh78ah@gmail.com>
2022-07-20 21:23:39 +0430

    :sparkles: included routers & added api tags

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from db.mongodb_utils import connect_to_mongo, close_mongo_connection
+from api.v1.router import router as apirouter
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "Product",
+        "description": "product CRUD",
+    }
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
 
 app.add_event_handler('startup', connect_to_mongo)
 app.add_event_handler('shutdown', close_mongo_connection)
+
+app.include_router(apirouter)
