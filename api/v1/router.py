97a6a36b6ea234b55d682fc54f4c488a659d80bf
AmirHossein <khanzadeh78ah@gmail.com>
2022-07-22 12:23:10 +0430

    :sparkles: Added user routes

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 from apps.product.views.productview import router as productrouter
 from apps.basket.views.basketview import router as basketrouter
+from apps.user.views.usercrud import router as usercrudrouter
+from apps.user.views.userauth import router as userauthrouter
 
 router = APIRouter()
 router.include_router(productrouter)
 router.include_router(basketrouter)
+router.include_router(usercrudrouter)
+router.include_router(userauthrouter)
