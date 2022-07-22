5decc845b7f378307ad440cd75b180b21b9126f0
AmirHossein <khanzadeh78ah@gmail.com>
2022-07-22 12:52:07 +0430

    :sparkles: Added authorization for user update and delete

--- apps/user/views/usercrud.py
+++ apps/user/views/usercrud.py
@@ -1,16 +1,11 @@
 from ..schema.userschema import UserIN, UserOut, UserInDB
 from db.mongodb import db
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from bson import ObjectId
 from passlib.hash import pbkdf2_sha256
+from apps.user.views.userauth import get_current_user
 
 router = APIRouter()
-
-
-@router.get('/user/{uid}', tags=["User CRUD"])
-async def retrieve_one(uid: str):
-    res = await db.client['bdokdb']['test_user'].find_one({"_id": ObjectId(uid)})
-    return UserOut(**res).dict()
 
 
 @router.post('/user', tags=["User CRUD"])
@@ -21,13 +16,17 @@ async def create_user(user: UserIN):
     return {'id': str(res.inserted_id), 'username': user.username}
 
 
-@router.put('/user/{uid}', tags=["User CRUD"])
-async def update_one(uid: str, user: UserIN):
-    res = await db.client['bdokdb']['test_user'].replace_one({"_id": ObjectId(uid)}, user.dict())
+@router.put('/user', tags=["User CRUD"])
+async def update_one(user: UserIN, current_user=Depends(get_current_user)):
+    c_user = UserOut(**current_user)
+    hashed_password = pbkdf2_sha256.hash(user.password1)
+    user_in_db = UserInDB(**user.dict(), hashed_password=hashed_password)
+    res = await db.client['bdokdb']['test_user'].replace_one({"username": c_user.username}, user_in_db.dict())
     return res.raw_result
 
 
-@router.delete('/user/{uid}', tags=["User CRUD"])
-async def delete_one(uid: str):
-    res = await db.client['bdokdb']['test_user'].delete_one({"_id": ObjectId(uid)})
+@router.delete('/user', tags=["User CRUD"])
+async def delete_one(current_user=Depends(get_current_user)):
+    user = UserOut(**current_user)
+    res = await db.client['bdokdb']['test_user'].delete_one({"username": user.username})
     return res.raw_result
